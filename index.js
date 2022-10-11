f2001f3d6df981c605da245bf6f0879df0520e56
Catherine <cpolknytx@gmail.com>
2022-10-11 06:59:21 -0400

    added nesessary files to index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+const generateHTML = require('./generateHTMLfile/generateHTML')
+
+//require team members progiles 
+const Manager = require('./classes/manager');
+const Engineer = require('./classes/engineer');
+const Internt = require('./classes/engineer');
+
+//important node modules 
+const fs = require('fs');
+const inquirer = require('inquirer');
