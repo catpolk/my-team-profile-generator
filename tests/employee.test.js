9a9bce1495fe9add7a172a43a031ebb5b51631f4
Catherine <cpolknytx@gmail.com>
2022-10-27 16:09:05 -0500

    added test for class employee

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -1,3 +1,31 @@
 //Constructor Employee
 const Employee = require('../classes/employee');
 
+describe("Employee", () => {
+    describe("questions", () => {
+        it("prompts a user to provide a name", () => {
+            const name = "Catherine";
+
+            const result = new Employee().questions(name);
+
+            expect(result).toEqual(name);
+        })
+    })
+
+        it("prompts a user to provide an id", () => {
+            const id = 01;
+
+            const result = new Employee().questions(id);
+
+            expext(result).toEqual(id);
+        })
+
+        it("prompts a user to provide an email", () => {
+            const email = "catherinetest@gmail.com";
+
+            const result = new Employee().questions(email);
+
+            expect(result).toEqual(email);
+        })
+
+})
